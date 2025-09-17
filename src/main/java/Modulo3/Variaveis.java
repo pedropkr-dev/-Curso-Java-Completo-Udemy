8a1abe3ca2886bd80a45bb3825eb90f8b5232b8d
pedropkr-dev <pedropkr3@gmail.com>
2025-09-17 20:02:00 -0300

    Aula de Variaveis

---
+++ src/main/java/Modulo3/Variaveis.java
@@ -0,0 +1,23 @@
+package Modulo3;
+
+import java.sql.SQLOutput;
+
+public class Variaveis {
+
+    //Para fazer uma constante,eu preciso utilizar o "static final". Observação: Diferentemente da variável, não é um valor mutável,
+    //O "float" serve para trabalhar com valores quebrados, ou seja, decimais.
+    static final float VALOR_PI = 3.14f;
+
+    public static void main(String[] args) {
+
+        //Variável é um valor volátil
+
+        int idade;
+        idade = 19;
+
+        idade = 20;
+
+        System.out.println("idade: " + idade);
+        System.out.println("valor de PI: "+ VALOR_PI);
+    }
+}
