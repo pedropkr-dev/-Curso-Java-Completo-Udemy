c96a422b0e74259102e765033957e9de10f6cd7e
pedropkr-dev <pedropkr3@gmail.com>
2025-09-19 12:51:07 -0300

    Exemplos de variaveis

---
+++ src/main/java/Modulo3/ExemplosVariaveis.java
@@ -0,0 +1,17 @@
+package Modulo3;
+
+public class ExemplosVariaveis {
+    public static void main(String[] args) {
+        int idade = 25;
+        double altura = 1.75;
+        char sexo = 'M';
+        boolean estaChovendo = false;
+        String nome = "Pedro";
+
+        System.out.println("idade: " + idade);
+        System.out.println("altura: " + altura);
+        System.out.println("sexo: " + sexo);
+        System.out.println("estaChovendo: " + estaChovendo);
+        System.out.println("nome: " + nome);
+    }
+}
