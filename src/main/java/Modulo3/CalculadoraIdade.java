a6635f4b16545ae09c8804dc48ca9e9b2b3d3d21
pedropkr-dev <pedropkr3@gmail.com>
2025-09-19 12:57:17 -0300

    calculadoraIdade

---
+++ src/main/java/Modulo3/CalculadoraIdade.java
@@ -0,0 +1,12 @@
+package Modulo3;
+
+public class CalculadoraIdade {
+    public static void main(String[] args) {
+
+        int anoAtual = 2025;
+        int anoNascimento = 1990;
+        int resultado =anoAtual - anoNascimento;
+
+        System.out.println("A idade é " + resultado + " anos");
+    }
+}
