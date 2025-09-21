6227c209efe6a0d2c43e5bbd48d59d693e0fabdd
pedropkr-dev <pedropkr3@gmail.com>
2025-09-21 00:49:02 -0300

    A40 - Declarando Arrays e acessando os elementos

---
+++ src/main/java/Modulo3/InicializacaoArrays.java
@@ -0,0 +1,19 @@
+package Modulo3;
+
+public class InicializacaoArrays {
+    public static void main(String[] args) {
+
+        String nome = "Pedro";
+        String telefone1 = "123489875";
+        String telefone2 = "423489875";
+        String[] filhos = {"Zé", "Maria", "Pedro", "Rita"};
+
+        int idade = 30;
+        int[] numerosImpares = {1, 3, 5, 7, 9};
+
+        System.out.println(filhos.[3]);
+        System.out.println(filhos.[2]);
+        System.out.println(filhos.[1]);
+        System.out.println(filhos.[0]);
+    }
+}
